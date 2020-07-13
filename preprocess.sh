3979a391b4e89c9014e8f8e7c69fb756480ac45b
JudePark96 <judepark@kookmin.ac.kr>
2020-07-13 18:47:20 +0900

    add requirements.txt

---
+++ preprocess.sh
@@ -0,0 +1,3 @@
+python3 preprocess.py --source_dataset '../rsc/kp20k/kp20k_validation.json' \
+                      --ground_truth_path '../rsc/prepro_dataset/kp20k.dev.cand.json' \
+                      --preprocess_path '../rsc/prepro_dataset/kp20k.dev.json'
