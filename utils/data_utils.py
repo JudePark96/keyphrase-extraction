25d5832dec0d8df23c0466c79cb21362b6a4566f
JudePark96 <judepark@kookmin.ac.kr>
2020-07-21 00:40:42 +0900

    added h5py save

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -10,6 +10,7 @@ import pickle
 
 from typing import Any
 
+import h5py as h5py
 import torch
 from tqdm import tqdm
 from transformers import BertTokenizer
@@ -22,6 +23,13 @@ def save_pickle_data(filename: str, data: Any) -> None:
     with codecs.open(filename, 'wb') as f:
         pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
         f.close()
+
+
+def load_pickle_data(filename: str) -> Any:
+    with codecs.open(filename, 'rb') as f:
+        data = pickle.load(f)
+        f.close()
+    print(data)
 
 
 def filter_absent_keyphrase(t: torch.Tensor) -> bool:
@@ -73,6 +81,9 @@ def get_and_save_dataset(
 
             squeezed_input_ids = encoded_doc_words['input_ids'].squeeze(dim=0)
 
+            start_pos_tensor = torch.zeros((1, max_doc_seq_len)).long()
+            end_pos_tensor = torch.zeros((1, max_doc_seq_len)).long()
+
             for keyphrase in keyphrases:
                 # the case of consisting of several words.
                 if len(keyphrase) > 1:
@@ -92,8 +103,8 @@ def get_and_save_dataset(
 
                         if filter_absent_keyphrase(s_p) and filter_absent_keyphrase(e_p):
                             if max_doc_seq_len > s_p[0].item() and max_doc_seq_len > e_p[0].item():
-                                start_pos.append(s_p[0].item())
-                                end_pos.append(e_p[0].item())
+                                start_pos_tensor[0][s_p[0].item()] = 1
+                                end_pos_tensor[0][s_p[0].item()] = 1
 
                 else:
                     # present keyphrase extraction
@@ -109,8 +120,8 @@ def get_and_save_dataset(
 
                             if filter_absent_keyphrase(s_p) and filter_absent_keyphrase(e_p):
                                 if max_doc_seq_len > s_p[0].item() and max_doc_seq_len > e_p[0].item():
-                                    start_pos.append(s_p[0].item())
-                                    end_pos.append(e_p[0].item())
+                                    start_pos_tensor[0][s_p[0].item()] = 1
+                                    end_pos_tensor[0][s_p[0].item()] = 1
                         else:
                             keyphrase_wo_special_tokens = decoded_keyphrase[1:len(decoded_keyphrase)-1]
                             s_p = (squeezed_input_ids == keyphrase_wo_special_tokens[0]).nonzero()
@@ -118,20 +129,64 @@ def get_and_save_dataset(
 
                             if filter_absent_keyphrase(s_p) and filter_absent_keyphrase(e_p):
                                 if max_doc_seq_len > s_p[0].item() and max_doc_seq_len > e_p[0].item():
-                                    start_pos.append(s_p[0].item())
-                                    end_pos.append(e_p[0].item())
+                                    start_pos_tensor[0][s_p[0].item()] = 1
+                                    end_pos_tensor[0][s_p[0].item()] = 1
 
             features.append({
                 'url': json_object['url'],
                 'title': encoded_title,
                 'doc_words': encoded_doc_words,
-                'start_position': start_pos,
-                'end_position': end_pos
+                'start_pos_tensor': start_pos_tensor,
+                'end_pos_tensor': end_pos_tensor
             })
+
+            if idx == 100:
+                break
 
         f.close()
 
-    save_pickle_data(output_path, features)
+    """
+    url => [1, 2, 3, ..., n]
+    title => [input_ids, token_input_ids, attention_mask]
+    doc_words => [input_ids, token_input_ids, attention_mask]
+    start_pos_tensor
+    end_pos_tensor
+    """
+
+    # label
+    start_pos_tensors = torch.cat([feature['start_pos_tensor'] for feature in features], dim=0)
+    end_pos_tensors = torch.cat([feature['end_pos_tensor'] for feature in features], dim=0) # [n x max_len], n is size of dataset.
+
+    # encoded_title
+    title_input_ids = torch.cat([feature['title']['input_ids'] for feature in features], dim=0)
+    title_token_type_ids = torch.cat([feature['title']['token_type_ids'] for feature in features], dim=0)
+    title_attention_mask = torch.cat([feature['title']['attention_mask'] for feature in features], dim=0)
+
+    # encoded_doc_words
+    doc_input_ids = torch.cat([feature['doc_words']['input_ids'] for feature in features], dim=0)
+    doc_token_type_ids = torch.cat([feature['doc_words']['token_type_ids'] for feature in features], dim=0)
+    doc_attention_mask = torch.cat([feature['doc_words']['attention_mask'] for feature in features], dim=0)
+
+    feature_output = h5py.File(output_path, 'w')
+
+    title_group = feature_output.create_group('title')
+    title_group.create_dataset('input_ids', data=title_input_ids)
+    title_group.create_dataset('token_type_ids', data=title_token_type_ids)
+    title_group.create_dataset('attention_mask', data=title_attention_mask)
+
+    doc_group = feature_output.create_group('doc')
+    doc_group.create_dataset('input_ids', data=doc_input_ids)
+    doc_group.create_dataset('token_type_ids', data=doc_token_type_ids)
+    doc_group.create_dataset('attention_mask', data=doc_attention_mask)
+
+    label_group = feature_output.create_group('label')
+    label_group.create_dataset('start_pos', data=start_pos_tensors)
+    label_group.create_dataset('end_pos', data=end_pos_tensors)
+
+    feature_output.close()
+
+    print('feature preprocessing is done.')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
