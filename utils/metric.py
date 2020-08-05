80e9670b4aefb6100f5f3f9604996b50e85c2df0
JudePark96 <judepark@kookmin.ac.kr>
2020-08-05 18:29:33 +0900

    added f1 score metric

---
+++ utils/metric.py
@@ -0,0 +1,34 @@
+__author__ = 'JudePark'
+__email__ = 'judepark@kookmin.ac.kr'
+
+
+import torch
+
+
+def f1_score(y_true: torch.Tensor, y_pred: torch.Tensor, is_training=False) -> torch.Tensor:
+    assert y_true.ndim == 1
+    assert y_pred.ndim == 1 or y_pred.ndim == 2
+
+    if y_pred.ndim == 2:
+        y_pred = y_pred.argmax(dim=1)
+
+    tp = (y_true * y_pred).sum().to(torch.float32)
+    tn = ((1 - y_true) * (1 - y_pred)).sum().to(torch.float32)
+    fp = ((1 - y_true) * y_pred).sum().to(torch.float32)
+    fn = (y_true * (1 - y_pred)).sum().to(torch.float32)
+
+    epsilon = 1e-7
+
+    precision = tp / (tp + fp + epsilon)
+    recall = tp / (tp + fn + epsilon)
+
+    f1 = 2 * (precision * recall) / (precision + recall + epsilon)
+    f1.requires_grad = is_training
+    return f1
+
+
+if __name__ == '__main__':
+    y_true = torch.randint(0, 2, (10,))
+    y_pred = torch.randint(0, 2, (10,))
+
+    print(f1_loss(y_true, y_pred))
